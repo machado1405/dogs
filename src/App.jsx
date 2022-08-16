7d58481771770ba5e311560d0741a1b1318854fe
Matheus Da Silva Machado <agromsmachado@gmail.com>
2022-08-16 15:56:38 -0300

    configuraço de router e pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
   return (
-    <div className="App">
-      <p>APP curso</p>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
