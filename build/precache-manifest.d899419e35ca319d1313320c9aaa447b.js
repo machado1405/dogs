b9f42fb28b6ba0156dd5c5d5726f2cbad6799917
Matheus Da Silva Machado <agromsmachado@gmail.com>
2022-09-06 12:31:58 -0300

    gh pages

---
+++ build/precache-manifest.d899419e35ca319d1313320c9aaa447b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e1f025fb57422664de7",
+    "url": "/static/css/main.6f6b7d11.chunk.css"
+  },
+  {
+    "revision": "9e1f025fb57422664de7",
+    "url": "/static/js/main.9e1f025f.chunk.js"
+  },
+  {
+    "revision": "131ed3b8fa0e3e8ef0ff",
+    "url": "/static/css/1.dd31e05a.chunk.css"
+  },
+  {
+    "revision": "131ed3b8fa0e3e8ef0ff",
+    "url": "/static/js/1.131ed3b8.chunk.js"
+  },
+  {
+    "revision": "39eef9a4b3186fe6c2ca",
+    "url": "/static/js/2.39eef9a4.chunk.js"
+  },
+  {
+    "revision": "a8696301889bf038a72a",
+    "url": "/static/js/3.a8696301.chunk.js"
+  },
+  {
+    "revision": "6eed321befc97a60b032",
+    "url": "/static/js/runtime~main.6eed321b.js"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/static/media/visualizacao.4ce15ed8.svg"
+  },
+  {
+    "revision": "824d7d85ae3af9a43a61d46fe174192e",
+    "url": "/static/media/login.824d7d85.jpg"
+  },
+  {
+    "revision": "849a64433f3bdd08d38623156960aa56",
+    "url": "/static/media/dogs.849a6443.svg"
+  },
+  {
+    "revision": "8cc31290254329d8d3990af08d454cd7",
+    "url": "/static/media/dogs-footer.8cc31290.svg"
+  },
+  {
+    "revision": "9f80ad3de4a46815a44aa1a78bfc37dd",
+    "url": "/static/media/enviar.9f80ad3d.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "4861dcf7158674e1bf7e6dfa4f913c41",
+    "url": "/index.html"
+  }
+];
