7bb8943e118e5fb05bcd242a88b86399d8e6eb04
Matheus Da Silva Machado <agromsmachado@gmail.com>
2022-09-06 08:42:40 -0300

    Ajustes finais de css e mensagens de erro

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -38,7 +38,7 @@ function LoginPasswordReset() {
   }, []);
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete sua senha" />
       <h1 className="title">Resete sua senha!</h1>
       <form onSubmit={handleSubmit}>
@@ -55,7 +55,7 @@ function LoginPasswordReset() {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 }
 
